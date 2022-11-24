1ab5fbd2b3d71a1853df99345b6936dc3f3acb84
Arterning <china_ningning@outlook.com>
2022-11-24 15:49:45 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Basic from "./components/form";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello huang ning !!</h1>
+      <h2>React love ning!</h2>
+      <Basic />
+    </div>
+  );
+}
